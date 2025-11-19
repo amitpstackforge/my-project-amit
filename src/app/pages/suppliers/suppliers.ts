56e11e935f683234a67db940d1bc1704026cc23c
amitpstackforge <amitp.stackforge@gmail.com>
2025-11-19 16:21:51 +0530

    TABS ADDED

--- src/app/pages/suppliers/suppliers.ts
+++ src/app/pages/suppliers/suppliers.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-suppliers',
-  imports: [],
+  imports: [CommonModule],   // ✅ THIS IS REQUIRED
   templateUrl: './suppliers.html',
   styleUrl: './suppliers.css',
 })
 export class SuppliersComponent {
+  activeTab: string = 'suppliers';   // default selected tab
+
 
 }
